240de033f5ef9ca613990b257aa20c77e430f301
sergiolvargas95 <65960993+sergiolvargas95@users.noreply.github.com>
2022-04-25 17:47:21 -0500

    adding the navbar component, favicon, components and pages

---
+++ src/app/pages/register-products/register-products.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-products',
+  templateUrl: './register-products.component.html',
+  styles: [
+  ]
+})
+export class RegisterProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
