03602ea02baf46fc9328145a284ee5c2c8e4d774
Diego Herrera <daherre0@gmail.com>
2022-04-27 17:15:25 -0500

    login and register components created with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from "./pages/register/register.component";
+
 import { HomeComponent } from './pages/home/home.component';
+
 import { RegisterProductsComponent } from './pages/register-products/register-products.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'register-products', component: RegisterProductsComponent },
+  { path: "login", component: LoginComponent, pathMatch: "full" },
+  { path: "register", component: RegisterComponent, pathMatch: "full" },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { RegisterProductsComponent } from './pages/register-products/register-products.component';
 import { FormsModule } from '@angular/forms';
 import { EcommerceService } from './services/EcommerceService.service';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { EcommerceService } from './services/EcommerceService.service';
     NavbarComponent,
     FooterComponent,
     HomeComponent,
-    RegisterProductsComponent
+    RegisterProductsComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
