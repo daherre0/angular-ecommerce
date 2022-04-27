3f03a0a7e2821ec361286870f44aa0d89facab47
Diego Herrera <daherre0@gmail.com>
2022-04-27 17:29:19 -0500

    login and register templates and css created and functions binding

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -3,14 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styles: [
-  ]
+  styleUrls: ['./register.component.css']
+
 })
 export class RegisterComponent implements OnInit {
+
+  email!: string;
+  password!: string;
+  confirmPassword!: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  register() {
+    console.log(this.email);
+    console.log(this.password);
+  }
 }
